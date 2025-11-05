876e7c39d013398d8c5d53dca41b514f4c3d38f9
DikshyaPaudel <dikshyapaudel9@gmail.com>
2025-11-05 14:58:39 +0545

    Updating Purchase Invoice api.py

--- avinashgroup_app/custom_code/api.py
+++ avinashgroup_app/custom_code/api.py
@@ -7,7 +7,7 @@ class CustomPurchaseInvoice(PurchaseInvoice):
         purchase_type = self.custom_purchase_type_code 
         doc_no = str(self.custom_document_no).zfill(5) if self.custom_document_no else "00000"
         fiscal_year = self.custom_fiscal_year
-        self.name = f"{company_code}-{purchase_type}-{doc_no}-{fiscal_year}"
+        # self.name = f"{company_code}-{purchase_type}-{doc_no}-{fiscal_year}"
         
         if self.is_return:
             self.name = f"{company_code}-RTN-{doc_no}-{fiscal_year}"
